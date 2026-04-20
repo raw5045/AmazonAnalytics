3aff21c4de178cb57abc6c8e0b32c663050f3909
raw5045 <raw5045@users.noreply.github.com>
2026-04-20 15:44:55 -0400

    feat(inngest): add batch import orchestrator

--- inngest/functions/index.ts
+++ inngest/functions/index.ts
@@ -1,5 +1,6 @@
 import { rubricUploadedFn } from './rubric';
 import { validateFileFn } from './validate';
 import { importFileFn } from './importFile';
+import { importBatchFn } from './importBatch';
 
-export const functions = [rubricUploadedFn, validateFileFn, importFileFn];
+export const functions = [rubricUploadedFn, validateFileFn, importFileFn, importBatchFn];
